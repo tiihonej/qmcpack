a09ce7053f9cdcbb47404ccecaf694f18292f582
rcclay <j.k.rofling@gmail.com>
2019-05-01 11:17:00 -0600

    Add template for VMC driver using guiding function

---
+++ src/QMCDrivers/VMC/VMCUpdatePbyPGuide.cpp
@@ -0,0 +1,138 @@
+//////////////////////////////////////////////////////////////////////////////////////
+// This file is distributed under the University of Illinois/NCSA Open Source License.
+// See LICENSE file in top directory for details.
+//
+// Copyright (c) 2016 Jeongnim Kim and QMCPACK developers.
+//
+// File developed by: Ken Esler, kpesler@gmail.com, University of Illinois at Urbana-Champaign
+//                    Jeremy McMinnis, jmcminis@gmail.com, University of Illinois at Urbana-Champaign
+//                    Jeongnim Kim, jeongnim.kim@gmail.com, University of Illinois at Urbana-Champaign
+//                    Jaron T. Krogel, krogeljt@ornl.gov, Oak Ridge National Laboratory
+//                    Mark A. Berrill, berrillma@ornl.gov, Oak Ridge National Laboratory
+//
+// File created by: Jeongnim Kim, jeongnim.kim@gmail.com, University of Illinois at Urbana-Champaign
+//////////////////////////////////////////////////////////////////////////////////////
+
+
+#include "QMCDrivers/VMC/VMCUpdatePbyPGuide.h"
+#include "QMCDrivers/DriftOperators.h"
+#include "Message/OpenMP.h"
+#if !defined(REMOVE_TRACEMANAGER)
+#include "Estimators/TraceManager.h"
+#else
+typedef int TraceManager;
+#endif
+
+namespace qmcplusplus
+{
+/// Constructor
+VMCUpdatePbyPGuide::VMCUpdatePbyPGuide(MCWalkerConfiguration& w, TrialWaveFunction& psi, QMCHamiltonian& h, RandomGenerator_t& rg)
+    : QMCUpdateBase(w, psi, h, rg)
+{
+  myTimers.push_back(new NewTimer("VMCUpdatePbyPGuide::Buffer", timer_level_medium));       //timer for MC, ratio etc
+  myTimers.push_back(new NewTimer("VMCUpdatePbyPGuide::MovePbyP", timer_level_medium));     //timer for MC, ratio etc
+  myTimers.push_back(new NewTimer("VMCUpdatePbyPGuide::Hamiltonian", timer_level_medium));  //timer for Hamiltonian
+  myTimers.push_back(new NewTimer("VMCUpdatePbyPGuide::Collectables", timer_level_medium)); //timer for measurements
+  for (int i = 0; i < myTimers.size(); ++i)
+    TimerManager.addTimer(myTimers[i]);
+}
+
+VMCUpdatePbyPGuide::~VMCUpdatePbyPGuide() {}
+
+void VMCUpdatePbyPGuide::advanceWalker(Walker_t& thisWalker, bool recompute)
+{
+  myTimers[0]->start();
+  W.loadWalker(thisWalker, true);
+  Walker_t::WFBuffer_t& w_buffer(thisWalker.DataSet);
+  Psi.copyFromBuffer(W, w_buffer);
+  myTimers[0]->stop();
+
+  // start PbyP moves
+  myTimers[1]->start();
+  bool moved = false;
+  constexpr RealType mhalf(-0.5);
+  for (int iter = 0; iter < nSubSteps; ++iter)
+  {
+    //create a 3N-Dimensional Gaussian with variance=1
+    makeGaussRandomWithEngine(deltaR, RandomGen);
+    moved = false;
+    for (int ig = 0; ig < W.groups(); ++ig) //loop over species
+    {
+      RealType tauovermass = Tau * MassInvS[ig];
+      RealType oneover2tau = 0.5 / (tauovermass);
+      RealType sqrttau     = std::sqrt(tauovermass);
+      for (int iat = W.first(ig); iat < W.last(ig); ++iat)
+      {
+        W.setActive(iat);
+        PosType dr;
+        if (UseDrift)
+        {
+          GradType grad_now = Psi.evalGrad(W, iat);
+          DriftModifier->getDrift(tauovermass, grad_now, dr);
+          dr += sqrttau * deltaR[iat];
+        }
+        else
+        {
+          dr = sqrttau * deltaR[iat];
+        }
+        if (!W.makeMoveAndCheck(iat, dr))
+        {
+          ++nReject;
+          continue;
+        }
+        RealType logGf(1), logGb(1), prob;
+        if (UseDrift)
+        {
+          GradType grad_new;
+          RealType ratio = Psi.ratioGrad(W, iat, grad_new);
+          prob           = ratio * ratio;
+          logGf          = mhalf * dot(deltaR[iat], deltaR[iat]);
+          DriftModifier->getDrift(tauovermass, grad_new, dr);
+          dr    = W.R[iat] - W.activePos - dr;
+          logGb = -oneover2tau * dot(dr, dr);
+        }
+        else
+        {
+          RealType ratio = Psi.ratio(W, iat);
+          prob           = ratio * ratio;
+        }
+        if (prob >= std::numeric_limits<RealType>::epsilon() && RandomGen() < prob * std::exp(logGb - logGf))
+        {
+          moved = true;
+          ++nAccept;
+          Psi.acceptMove(W, iat);
+          W.acceptMove(iat);
+        }
+        else
+        {
+          ++nReject;
+          W.rejectMove(iat);
+          Psi.rejectMove(iat);
+        }
+      }
+    }
+    Psi.completeUpdates();
+  }
+  W.donePbyP();
+  myTimers[1]->stop();
+  myTimers[0]->start();
+  RealType logpsi = Psi.updateBuffer(W, w_buffer, recompute);
+  W.saveWalker(thisWalker);
+  myTimers[0]->stop();
+  // end PbyP moves
+  myTimers[2]->start();
+  EstimatorRealType eloc = H.evaluate(W);
+  thisWalker.resetProperty(logpsi, Psi.getPhase(), eloc);
+  myTimers[2]->stop();
+  myTimers[3]->start();
+  H.auxHevaluate(W, thisWalker);
+  H.saveProperty(thisWalker.getPropertyBase());
+  myTimers[3]->stop();
+#if !defined(REMOVE_TRACEMANAGER)
+  Traces->buffer_sample(W.current_step);
+#endif
+  if (!moved)
+    ++nAllRejected;
+}
+
+} // namespace qmcplusplus
---
+++ src/QMCDrivers/VMC/VMCUpdatePbyPGuide.h
@@ -0,0 +1,41 @@
+//////////////////////////////////////////////////////////////////////////////////////
+// This file is distributed under the University of Illinois/NCSA Open Source License.
+// See LICENSE file in top directory for details.
+//
+// Copyright (c) 2016 Jeongnim Kim and QMCPACK developers.
+//
+// File developed by: Ken Esler, kpesler@gmail.com, University of Illinois at Urbana-Champaign
+//                    Jeongnim Kim, jeongnim.kim@gmail.com, University of Illinois at Urbana-Champaign
+//                    Jeremy McMinnis, jmcminis@gmail.com, University of Illinois at Urbana-Champaign
+//                    Mark A. Berrill, berrillma@ornl.gov, Oak Ridge National Laboratory
+//
+// File created by: Jeongnim Kim, jeongnim.kim@gmail.com, University of Illinois at Urbana-Champaign
+//////////////////////////////////////////////////////////////////////////////////////
+
+
+#ifndef QMCPLUSPLUS_VMC_PARTICLEBYPARTICLE_GUIDE_UPDATE_H
+#define QMCPLUSPLUS_VMC_PARTICLEBYPARTICLE_GUIDE_UPDATE_H
+#include "QMCDrivers/QMCUpdateBase.h"
+
+namespace qmcplusplus
+{
+/** @ingroup QMCDrivers  ParticleByParticle
+ *@brief Implements the VMC algorithm using particle-by-particle move.
+ */
+class VMCUpdatePbyPGuide : public QMCUpdateBase
+{
+public:
+  /// Constructor.
+  VMCUpdatePbyPGuide(MCWalkerConfiguration& w, TrialWaveFunction& psi, QMCHamiltonian& h, RandomGenerator_t& rg);
+
+  ~VMCUpdatePbyPGuide();
+
+  void advanceWalker(Walker_t& thisWalker, bool recompute);
+
+private:
+  std::vector<NewTimer*> myTimers;
+};
+
+} // namespace qmcplusplus
+
+#endif
